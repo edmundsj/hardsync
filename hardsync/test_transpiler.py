a0cb7dc07784f9c2af4feae92c94777ddcaa3b4a
Jordan <edmundsj@uci.edu>
2023-08-19 13:29:56 -0400

    implemented standard C++ to arduino-C++ transpiler

--- hardsync/test_transpiler.py
+++ hardsync/test_transpiler.py
@@ -1,56 +1,142 @@
 import pytest
-from hardsync.transpiler import extract_variable_values, transpile  # Make sure to import the function
+from hardsync.transpiler import (
+    template_to_regex, var_names_from_template, populate_template, transpile_template,
+    transpile, cpp_to_arduino
+)
 
 
-def test_basic_functionality():
-    template = "myFunc({{var1}}, {{var2}}, {{var3}})"
-    actual = "myFunc('hello', 'world', 'test')"
-    expected = {'var1': "'hello'", 'var2': "'world'", 'var3': "'test'"}
-    assert extract_variable_values(template, actual) == expected
-
-def test_outside_function():
-    template = "yolo{{var1}}"
-    actual = "yoloswag"
-    expected = {'var1': "swag"}
-    assert extract_variable_values(template, actual) == expected
+def test_template_to_regex_novar():
+    template = 'variable'
+    expected = 'variable'
+    actual = template_to_regex(template)
+    assert actual == expected
 
 
-def test_unequal_vars_and_values():
-    template = "myFunc({{var1}}, {{var2}})"
-    actual = "myFunc('hello', 'world', 'test')"
-    assert extract_variable_values(template, actual) is None
+def test_template_to_regex_single():
+    template = 'variable{{hi}}'
+    expected = r'variable(?P<hi>\S+)'
+    actual = template_to_regex(template)
+    assert actual == expected
 
 
-def test_different_function_names():
-    template = "myFunc1({{var1}}, {{var2}})"
-    actual = "myFunc2('hello', 'world')"
-    assert extract_variable_values(template, actual) is None
+def test_template_to_regex_double():
+    template = 'variable{{hi}}other{{there}}'
+    expected = r'variable(?P<hi>\S+)other(?P<there>\S+)'
+    actual = template_to_regex(template)
+    assert actual == expected
 
 
-def test_no_variables():
-    template = "myFunc()"
-    actual = "myFunc()"
-    assert extract_variable_values(template, actual) is None
+def test_var_names_from_template_one():
+    template = 'variable{{hi}}'
+    expected = ['hi']
+    actual = var_names_from_template(template)
+    assert actual == expected
 
 
-def test_non_matching_brackets():
-    template = "myFunc({{var1}, {{var2}})"
-    actual = "myFunc('hello', 'world')"
-    assert extract_variable_values(template, actual) is None
+def test_var_names_from_template_two():
+    template = 'variable{{hi}}other{{there}}'
+    expected = ['hi', 'there']
+    actual = var_names_from_template(template)
+    assert actual == expected
+
+
+def test_populate_template_novars():
+    template = 'variable'
+    desired = 'variable'
+    actual = populate_template(template=template, replacements={})
+    assert actual == desired
+
+
+def test_populate_template_single():
+    template = 'variable{{yolo}}'
+    desired = 'variableswag'
+    actual = populate_template(template=template, replacements={'yolo': 'swag'})
+    assert actual == desired
+
+
+def test_populate_template_two():
+    template = 'variable{{yolo}}othervar{{swag}}'
+    desired = 'variableswagothervarhello'
+    actual = populate_template(template=template, replacements={'yolo': 'swag', 'swag': 'hello'})
+    assert actual == desired
+
+
+def test_transpile_template_novars():
+    original_text = 'variable 1400'
+    template = 'variable'
+    replacement = 'swag'
+    desired = 'swag 1400'
+    actual = transpile_template(template=template, replacement_template=replacement, input_text=original_text)
+    assert actual == desired
+
+
+def test_transpile_template_single_var():
+    original_text = 'variable 1400'
+    template = 'variable {{val}}'
+    replacement = '{{val}} variable'
+    desired = '1400 variable'
+    actual = transpile_template(template=template, replacement_template=replacement, input_text=original_text)
+    assert actual == desired
+
+
+def test_transpile_template_two_vars():
+    original_text = 'variable 1400 1900'
+    template = 'variable {{val1}} {{val2}}'
+    replacement = '{{val1}} variable {{val2}}'
+    desired = '1400 variable 1900'
+    actual = transpile_template(template=template, replacement_template=replacement, input_text=original_text)
+    assert actual == desired
+
+
+def test_transpile_template_function():
+    original_text = 'myFunc(1400, 1900)'
+    template = 'myFunc({{val1}}, {{val2}})'
+    replacement = 'myFunc({{val2}}, {{val1}})'
+    desired = 'myFunc(1900, 1400)'
+    actual = transpile_template(template=template, replacement_template=replacement, input_text=original_text)
+    assert actual == desired
+
+
+def test_transpile_template_import():
+    original_text = '#import <string>'
+    template = '#import <string>'
+    replacement = '#import <WString.h>'
+    desired = '#import <WString.h>'
+    actual = transpile_template(template=template, replacement_template=replacement, input_text=original_text)
+    assert actual == desired
 
 
 
-def test_full_transpile_no_vars():
-    mapping = {"<string>": "<Wstring.hhh>"}
-    input_text = "#include <string>"
-    desired_text = "#include <Wstring.hhh>"
-    actual_text = transpile(content=input_text, mapping=mapping)
-    assert actual_text == desired_text
+def test_transpile_switch_vars():
+    original_text = 'myFunc(1400, 1200)'
+    mapping = {r'myFunc({{val1}}, {{val2}})': 'myFunc({{val2}}, {{val1}})'}
+    desired = 'myFunc(1200, 1400)'
+    actual = transpile(template_mapping=mapping, input_text=original_text)
+    assert desired == actual
 
 
-def test_full_transpile_one_var():
-    mapping = {"myFunc({{var}})": "myFunc(2*{{var}})"}
-    input_text = "myFunc(hello)"
-    desired_text = "myFunc(2*hello)"
-    actual_text = transpile(content=input_text, mapping=mapping)
-    assert actual_text == desired_text
+def test_transpile_cpp_to_arduino_substr():
+    original_text = "string.substr(start, count)"
+    mapping = cpp_to_arduino
+    desired = 'string.substring(start, start + count)'
+    actual = transpile(template_mapping=mapping, input_text=original_text)
+    assert desired == actual
+
+
+def test_transpile_cpp_to_arduino_program():
+    original_text = \
+"""
+#include <string>
+
+std::string newString = oldString.substr(start, end)
+"""
+    desired = \
+"""
+#include <WString.h>
+
+String newString = oldString.substring(start, start + end)
+"""
+    mapping = cpp_to_arduino
+    actual = transpile(template_mapping=mapping, input_text=original_text)
+    assert desired == actual
+
--- hardsync/transpiler.py
+++ hardsync/transpiler.py
@@ -2,52 +2,78 @@
 This takes generic C++ code and converts it into the equivalent arduino-C code.
 This transpiler can probably afford to be fragile, at least in the beginning, as we
 have complete control over both the code being transpiled and the transpiler.
+
+TODO: Deal with whitespace matching, add tests
+TODO: Figure out when exactly to throw errors in transpilation.
+
 """
-from typing import Mapping
+from typing import Mapping, Dict
 import re
 
 
 cpp_to_arduino = {
     '#include <string>': '#include <WString.h>',
     'std::string': 'String',
-    '.substr({{start}},{{count}})': '.substring({{start}},{{start}}+{{count}})',
+    '.substr({{start}}, {{count}})': '.substring({{start}}, {{start}} + {{count}})',
     '.find(': '.indexOf(',
 }
 
-def extract_variable_values(template, input_text):
-    # Extract the function name and all variables from the template string
-    match = re.match(r"([a-zA-Z_]\w*)\((.*?)\)", template)
-    if not match:
-        return None
 
-    func_name, var_sequence = match.groups()
-    var_names = re.findall(r"\{\{(\w+)\}\}", var_sequence)
+def template_to_regex(template: str):
+    pattern = r'{{(?P<name>\w+)}}'
+    matches = re.finditer(pattern=pattern, string=template)
+    new_string = template
 
-    # Build a regex pattern for the actual string
-    pattern = r"{}\((.*?)\)$".format(func_name)
+    # Replace all regex special characters
+    new_string = new_string.replace('(', r'\(')
+    new_string = new_string.replace(')', r'\)')
 
-    match = re.match(pattern, input_text)
-    if not match:
-        return None
-
-    # Extracting all values within the parentheses of the actual string
-    values = [v.strip() for v in match.group(1).split(',')]
-
-    # Check if the number of variables matches the number of values
-    if len(var_names) != len(values):
-        return None
-
-    return dict(zip(var_names, values))
+    for match in matches:
+        name = match.group('name')
+        new_regex = r'(?P<' + name + r'>\\S+)'
+        new_string = re.sub(pattern=pattern, repl=new_regex, string=new_string, count=1)
 
 
-def transpile(content: str, mapping: Mapping) -> str:
-    replacement_string = content
-    for key, val in mapping.items():
-        if '{{' not in key:
-            replacement_string = replacement_string.replace(key, val)
-            continue
+    return new_string
 
-        variables_to_replace = extract_variable_values(template=key, input_text=replacement_string)
-        breakpoint()
 
-    return replacement_string
+def var_names_from_template(template: str):
+    var_names = []
+    pattern = r'{{(?P<name>\w+)}}'
+    matches = re.finditer(pattern=pattern, string=template)
+    for match in matches:
+        name = match.group('name')
+        var_names.append(name)
+    return var_names
+
+
+def populate_template(template: str, replacements: Mapping[str, str]) -> str:
+    populated_template = template
+    for key, val in replacements.items():
+        var_to_match = '{{' + key + '}}'
+        populated_template = populated_template.replace(var_to_match, val)
+
+    return populated_template
+
+
+def transpile_template(template: str, replacement_template: str, input_text: str) -> Dict[str, str]:
+    regex = template_to_regex(template)
+    matches = re.finditer(pattern=regex, string=input_text)
+    var_names = var_names_from_template(template)
+
+    modified_text = input_text
+    for match in reversed(list(matches)): # Reverse the list so we are mutating the string from the end
+        replacements = {var: match.group(var) for var in var_names}
+        replacement_string = populate_template(replacement_template, replacements=replacements)
+        modified_text = modified_text[0:match.start()] + replacement_string + modified_text[match.end():]
+
+    return modified_text
+
+
+def transpile(input_text: str, template_mapping: Mapping[str, str]) -> str:
+    modified_text = input_text
+    for template, replacement_template in template_mapping.items():
+        modified_text = transpile_template(template=template, replacement_template=replacement_template, input_text=modified_text)
+    return modified_text
+
+
