65396243dca6c406136189152ae2b7a4c3c009e0
Jordan <edmundsj@uci.edu>
2023-08-25 08:35:08 -0400

    added wrapper + virtual declaration generators, need to add tests + edge case tests

--- README.md
+++ README.md
@@ -110,6 +110,7 @@ This library is based on simple request/response-based communication. The *clien
 - Add "ping"-based auto-discovery of serial devices
 - Add example with how to override baud rate and device serial number
 - Fix the type mapping defaults, add tests
+- Add tests for zero-length requests and responses, ensure void declarations are generated
 - [DONE] Move exchange to top-level of contract
 - [DONE] Fix code generation so that it generates in the current directory, not the module directory.
 - [DONE] Publish package to PyPi
--- hardsync/generators/arduino/arduino.py
+++ hardsync/generators/arduino/arduino.py
@@ -12,10 +12,36 @@ TEMPLATE_DIR = dir_name / 'templates'
 
 
 def virtual_declaration(exchange: Type[Exchange], type_mapping: TypeMapping):
-    pass
+    class_name = exchange.identifier()
+    cpp_code = "virtual "
+
+    response_fields = fields(exchange.Response)
+    if len(response_fields) > 1:
+        raise ValueError(f"Unspported number of response fields: {len(fields(exchange.Response))} only support 1 field")
+
+    if len(response_fields) == 1:
+        response_field = fields(exchange.Response)[0]
+        cpp_code += type_mapping[response_field.type] + " "
+    else:
+        cpp_code += "void "
+
+    cpp_code += f"{class_name.lower()}("
+    cpp_code += ', '.join([f"{type_mapping[field.type]} {field.name}" for field in fields(exchange.Request)])
+
+    cpp_code += ") const;"
+    return cpp_code
+
 
 def wrapper_declaration(exchange: Type[Exchange], type_mapping: TypeMapping):
-    pass
+    class_name = exchange.identifier()
+    cpp_code = "void "
+
+    cpp_code += f"{class_name.lower()}Wrapper("
+    cpp_code += ', '.join([f"{type_mapping[field.type]} {field.name}" for field in fields(exchange.Request)])
+
+    cpp_code += ") const;"
+    return cpp_code
+
 
 def respond_invocation(exchange: Type[Exchange], type_mapping: TypeMapping):
     class_name = exchange.identifier()
@@ -25,16 +51,10 @@ def respond_invocation(exchange: Type[Exchange], type_mapping: TypeMapping):
 
     for field in fields(exchange.Request):
 
-        if type_mapping[field.type] == 'int':
-            cpp_code += f'{ARDUINO_INDENT}int {field.name} = extractInt(&fn, "{field.name}");\n'
-        elif type_mapping[field.type] == 'double':
-            cpp_code += f'{ARDUINO_INDENT}double {field.name} = extractDouble(&fn, "{field.name}");\n'
-        elif type_mapping[field.type] == 'float':
-            cpp_code += f'{ARDUINO_INDENT}double {field.name} = extractFloat(&fn, "{field.name}");\n'
-        elif type_mapping[field.type] == 'String':
-            cpp_code += f'{ARDUINO_INDENT}String {field.name} = extractString(&fn, "{field.name}");\n'
-        else:
-            raise TypeError(f"Unsupported field type: {field.type}")
+        cpp_code += (
+            f'{ARDUINO_INDENT}{type_mapping[field.type]} {field.name} = extract'
+            f'{type_mapping[field.type].capitalize()}(&fn, "{field.name}");\n'
+        )
 
     cpp_code += (f'{ARDUINO_INDENT}this->{function_name}Wrapper'
                  f'({", ".join([field.name for field in fields(exchange.Request)])});\n')
