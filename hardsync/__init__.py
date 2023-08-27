285739b8b661907085c80f611ee3d931caf96224
Jordan <edmundsj@uci.edu>
2023-08-27 13:45:36 -0400

    moved tests into separate subfolder to avoid cluttering main directory

--- hardsync/__init__.py
+++ hardsync/__init__.py
@@ -5,5 +5,5 @@ __version__ = "0.3.0"
 __hash__ = "fbf800c3ca0fe01cf17b775d33557dc501ddf8f4"
 root_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 cpp_src_dir = root_dir / 'cpp' / 'src'
-test_data_dir = root_dir / 'test_data'
+test_data_dir = root_dir / 'tests' / 'test_data'
 
