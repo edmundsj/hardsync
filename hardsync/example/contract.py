e03f1174442330800b0c16049e63ae098874d9c5
Jordan <edmundsj@uci.edu>
2023-08-27 08:45:29 -0400

    added potential template for what Host/Device channel definition looks like

--- hardsync/example/contract.py
+++ hardsync/example/contract.py
@@ -7,4 +7,12 @@ class MeasureVoltage:
         voltage: float
 
 
+class Channel:
+    class Host:
+        write_bytes = "serial.write"
+        read_until = "serial.read_until"
+    class Device:
+        write_bytes = "Serial.print"
+        read_until = "Serial.read"
+
 # TODO: Add example for how to add a device-initiated exchange
